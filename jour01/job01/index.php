bab0edb19e9f817960148094f1a6b2b56a4df31b
Thibault <thibault.kine@laplateforme.io>
2021-11-02 10:26:29 +0100

    added job00 and job01, both done!

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -3,4 +3,14 @@ $str = "LaPlateforme";
 $str2 = "Vive";
 $str3 = "!";
 echo $str2." ".$str.$str3;
+
+$val = 6;
+echo $val;
+$val += 4;
+echo $val;
+
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
 ?>
